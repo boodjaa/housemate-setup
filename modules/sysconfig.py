15ac1fe0644e1ed6d05c3113901e275e7fea7ac8
Jamie Gostin <jamig@Jamies-MacBook-Air.local>
2026-06-17 20:04:11 +1000

    Hostname implementation

--- modules/sysconfig.py
+++ modules/sysconfig.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import re
-
 from modules.base import Module, ModuleError
 
 
@@ -9,8 +7,18 @@ class SysconfigModule(Module):
     name = "system"
     required = True
 
-    # -- install -----------------------------------------------------------
+    def validate(self) -> None:
+        pass
+    
     def install(self) -> None:
+        pass
 
+    def configure(self) -> None:
         # Set hostname
         self.runner.run(["hostnamectl", "set-hostname", self.settings["hostname"]])
+
+    def enable(self) -> None:
+        pass
+
+    def status(self) -> None:
+        pass
